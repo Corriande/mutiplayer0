c14acd800ae045481e9ed5f0544c23f363fb0534
Corriande <gabrielxdelvalle@gmail.com>
2025-04-30 12:27:12 -0400

    update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,7 @@
 let socket;
 let circle = { x: 200, y: 200 };
+let lastSend = 0;
+let prevX = -1, prevY = -1;
 
 function setup() {
   createCanvas(400, 400);
@@ -15,33 +17,15 @@ function setup() {
 function draw() {
   background(240);
 
-  let prevX = -1, prevY = -1;
-
-  function draw() {
-    if (mouseX !== prevX || mouseY !== prevY) {
-      if (millis() - lastSend > 33) {
-        socket.emit('move', { x: mouseX, y: mouseY });
-        lastSend = millis();
-        prevX = mouseX;
-        prevY = mouseY;
-      }
-    }
-
-  background(240);
-  ellipse(circle.x, circle.y, 50, 50);
-}
-
-
   fill(50, 100, 255);
   noStroke();
   ellipse(circle.x, circle.y, 50, 50);
-}
 
-function mouseMoved() {
-  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-    circle.x = mouseX;
-    circle.y = mouseY;
+  // Only send position if it changed and throttle to ~30fps
+  if ((mouseX !== prevX || mouseY !== prevY) && millis() - lastSend > 33) {
+    socket.emit('move', { x: mouseX, y: mouseY });
+    lastSend = millis();
+    prevX = mouseX;
+    prevY = mouseY;
   }
 }
-
-
